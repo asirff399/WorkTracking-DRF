374fcd406eef82ab6381a3c28eab981190d06087
Asir Intisar <aintisar48@gmail.com>
2025-07-06 11:39:09 +0600

    [Add urls]

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,21 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import UserRegisterAPIView, UserLoginAPIView, activate, UserLogoutAPIView, UserViewset
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('users', UserViewset)
+
 urlpatterns = [
-    # path('', ),
+    path('', include(router.urls)),
+    path('register', UserRegisterAPIView.as_view(), name='register'),
+    path('login', UserLoginAPIView.as_view(), name='login'),
+    path('logout', UserLogoutAPIView.as_view(), name='logout'),
+    path('activate/<uid64>/<token>/', activate, name='activate'),
+    
     # JWT
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
