4b046f81a90fee82aea087833c85c84889d26248
Asir Intisar <aintisar48@gmail.com>
2025-07-07 09:54:19 +0600

    [Model - Attendance][Serializers - AttendanceSerializer]

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from datetime import timedelta
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Attendance(models.Model):
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=[('present', 'Present'), ('absent', 'Absent')])
+    entry_time = models.TimeField(null=True, blank=True)
+    exit_time = models.TimeField(null=True, blank=True)
+    total_hours = models.DurationField(null=True, blank=True)
+    is_late = models.BooleanField(default=False)
+    is_early_leave = models.BooleanField(default=False)
+    
+    def calculate_total_hours(self):
+        if self.entry_time and self.exit_time:
+            entry = timedelta(hours=self.entry_time.hour, minutes=self.entry_time.minute, seconds=self.entry_time.second)
+            exit = timedelta(hours=self.exit_time.hour, minutes=self.exit_time.minute, seconds=self.exit_time.second)
+            self.total_hours = exit - entry
+            self.save()
+            
+            
+    
---
+++ attendance/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Attendance
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = ['id', 'employee', 'data', 'status', 'entry_time', 'exit_time', 'total_hours', 'is_late', 'is_early_leave']
+        read_only_fields = ['total_hours', 'is_late', 'is_early_leave','date']
+        
+        
